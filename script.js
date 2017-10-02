8e391fb4d551a3ebc54bdab9c384076fee9c757f
strawbee <yuejiao.hou@gmail.com>
2017-10-02 15:42:33 -0700

    Fairly complete

--- script.js
+++ script.js
@@ -35,17 +35,25 @@ var image1El = document.getElementById('image1');
 var image2El = document.getElementById('image2');
 var image3El = document.getElementById('image3');
 
-image1El.addEventListener('click', displayImages);
-image2El.addEventListener('click', displayImages);
-image3El.addEventListener('click', displayImages);
-
 var noDisplay1, noDisplay2, noDisplay3;
 var displayCounter = 0;
 
-function displayImages() {
-  var randomIndex1 = Math.floor(Math.random() * RandomImages.all.length);
-  var randomIndex2 = Math.floor(Math.random() * RandomImages.all.length);
-  var randomIndex3 = Math.floor(Math.random() * RandomImages.all.length);
+// Function Called When Image Is Clicked
+function displayImages(e) {
+  var target = e.target;
+  if (target == image1El) {
+    RandomImages.all[randomIndex1].votes += 1;
+  }
+  else if (target == image2El) {
+    RandomImages.all[randomIndex2].votes += 1;
+  }
+  else if (target == image3El) {
+    RandomImages.all[randomIndex3].votes += 1;
+  }
+
+  randomIndex1 = Math.floor(Math.random() * RandomImages.all.length);
+  randomIndex2 = Math.floor(Math.random() * RandomImages.all.length);
+  randomIndex3 = Math.floor(Math.random() * RandomImages.all.length);
 
   while(randomIndex3 === noDisplay1 || randomIndex3 === noDisplay2 || randomIndex3 === noDisplay3) {
     randomIndex3 = Math.floor(Math.random() * RandomImages.all.length);
@@ -73,18 +81,44 @@ function displayImages() {
 
   displayCounter++;
   if (displayCounter === 25) {
+    var randomImagesContent = document.getElementById('randomImages');
+    randomImagesContent.innerHTML = 'You have voted 25 times: <br />';
+    console.log('VIEWS AND VOTES:');
+    for (var i = 0; i < RandomImages.all.length; i++) {
+      randomImagesContent.innerHTML += RandomImages.all[i].name + ': ' + RandomImages.all[i].views + ' views || ' + RandomImages.all[i].votes + ' votes<br />';
+      console.log(RandomImages.all[i].name + ': ' + RandomImages.all[i].views + ' views || ' + RandomImages.all[i].votes + ' votes');
+    }
   }
-
-/*
-  if (num === 1) {
-    RandomImages.all[randomIndex1].votes += 1;
-  }
-  else if (num === 2) {
-    RandomImages.all[randomIndex2].votes += 1;
-  }
-  else if (num === 3) {
-    RandomImages.all[randomIndex3].votes += 1;
-  }
-  */
 }
-displayImages();
+
+// Display Initial Random Images
+
+var randomIndex1 = Math.floor(Math.random() * RandomImages.all.length);
+var randomIndex2 = Math.floor(Math.random() * RandomImages.all.length);
+var randomIndex3 = Math.floor(Math.random() * RandomImages.all.length);
+
+while (randomIndex2 === randomIndex3) {
+  randomIndex2 = Math.floor(Math.random() * RandomImages.all.length);
+}
+
+while (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3) {
+  randomIndex1 = Math.floor(Math.random() * RandomImages.all.length);
+}
+
+image1El.src = RandomImages.all[randomIndex1].url;
+image2El.src = RandomImages.all[randomIndex2].url;
+image3El.src = RandomImages.all[randomIndex3].url;
+
+noDisplay1 = randomIndex1;
+noDisplay2 = randomIndex2;
+noDisplay3 = randomIndex3;
+
+RandomImages.all[randomIndex1].views += 1;
+RandomImages.all[randomIndex2].views += 1;
+RandomImages.all[randomIndex3].views += 1;
+
+// Event Listeners
+
+image1El.addEventListener('click', displayImages);
+image2El.addEventListener('click', displayImages);
+image3El.addEventListener('click', displayImages);
